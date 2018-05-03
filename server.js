465a2917693402afe05e4bc54249ec7a673605e3
Martin Helfer <martin-helfer@gmx.de>
2018-05-03 16:26:55 +0200

    New first commit

--- server.js
+++ server.js
@@ -1,74 +1,150 @@
-'use strict';
+//WebSocketServer anlegen
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080, clientTracking: true });
 
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
-
+//Befehle auf Kommandzeile ausfuehren
 const { execSync } = require('child_process');
 
-let timerId = null;
-let sockets = new Set();
+//Timer benachrichtigt in regelmaesigen Abstaenden ueber Aenderung z.B. Zeit
+timerID = null;
 
-//Bei Verbindung eines Sockets
-io.on('connection', (socket) => {
+//Aktuellen Song / Volume / MuteStatus merken, damit Clients, die sich spaeter anmelden, diese Info bekommen
+currentSong = null;
+currentVolume = 100;
+currentMute = false;
 
-    //Socket in Liste einfuegen
-    sockets.add(socket)
-    console.log(`Socket ${socket.id} added`);
+//Wenn sich ein WebSocket mit dem WebSocketServer verbindet
+wss.on('connection', function connection(ws) {
+    console.log("new client connected");
 
-    //Wenn Timer noch nicht laueft -> Timer starten
-    if (!timerId) {
+    //Timer starten, falls er nicht laeuft
+    if (!timerID) {
         startTimer();
     }
 
-    //Wenn der Socket eine "set-volume" Message sendet
-    socket.on('set-volume', (volume) => {
+    //Wenn WebSocket eine Nachricht sendet
+    ws.on('message', function incoming(message) {
 
-        //Diesen Wert an Sockets verteilen
-        console.log('SET VOLUME TO ' + volume);
-        io.emit('send-volume-message', volume);
+        //Nachricht kommt als String -> in JSON Objekt konvertieren
+        var obj = JSON.parse(message);
+        console.log(obj)
+
+        //Werte auslesen
+        let type = obj.type;
+        let value = obj.value;
+
+        //Array von MessageObjekte erstellen, die an WS gesendet werden
+        let messageObjArr = [{
+            type: type,
+            value: value
+        }];
+
+        //Pro Typ gewisse Aktionen durchfuehren
+        switch (type) {
+
+            //Song hat sich geandert
+            case 'song-change':
+                console.log("New Song is " + value);
+
+                //neue Song merken
+                currentSong = value;
+                break;
+
+            //Playback wurde beendet
+            case 'stop':
+                console.log("STOPPED")
+                break;
+
+            //Lautstaerke setzen
+            case 'set-volume':
+
+                //neue Lautstaerke merken 
+                currentVolume = value;
+
+                //Mute (ggf.) entfernen und Lautstaerke setzen
+                let volumeCommand = "sudo amixer sset Master on && sudo amixer sset Master " + value + "% -M";
+                console.log(volumeCommand)
+                execSync(volumeCommand);
+
+                //Zusaetzliche Nachricht an clients, dass nun nicht mehr gemutet ist
+                messageObjArr.push({
+                    type: "set-mute",
+                    value: false
+                })
+                break;
+
+            //Mute-Status setzen
+            case 'set-mute':
+
+                //neuen Mutestatus merken 
+                currentMute = value;
+
+                //Mute fuer Mixer ermitteln
+                muteState = value ? "off" : "on";
+
+                //Lautstaerke setzen
+                let muteCommand = "sudo amixer sset Master " + muteState;
+                console.log(muteCommand)
+                execSync(muteCommand);
+                break;
+        }
+
+        //Ueber Liste der MessageObjs gehen und an WS senden
+        for (ws of wss.clients) {
+            for (messageObj of messageObjArr)
+                ws.send(JSON.stringify(messageObj));
+        }
     });
 
-    //Beim disconnect eines Sockets
-    socket.on('disconnect', function () {
+    //WS (einmalig beim Verbinden) ueber aktuellen Titel informieren
+    ws.send(JSON.stringify({
+        type: "song-change",
+        value: currentSong
+    }));
 
-        //Socket aus Liste werfen
-        console.log(`Deleting socket: ${socket.id}`);
-        sockets.delete(socket);
-        console.log(`Remaining sockets: ${sockets.size}`);
-    });
+    //WS (einmalig beim Verbinden) ueber aktuelles Volume informieren
+    ws.send(JSON.stringify({
+        type: "set-volume",
+        value: currentVolume
+    }));
+
+    //WS (einmalig beim Verbinden) ueber aktuellen Mutezustand informieren
+    ws.send(JSON.stringify({
+        type: "set-mute",
+        value: currentMute
+    }));
 });
 
-//Timer Funktion liefert in regelmaessigen Abstaenden Werte an alle Sockets
+//Timer-Funktion benachrichtigt regelmaessig die WS
 function startTimer() {
-    console.log("start timer");
-    timerId = setInterval(() => {
+    console.log("startTimer")
 
-        //Wenn es keine Sockets mehr gibt -> Timer-Funktion stoppen
-        if (!sockets.size) {
-            clearInterval(timerId);
-            timerId = null;
-            console.log(`Timer stopped`);
+    //TimerID, damit Timer zurueckgesetzt werden kann
+    timerID = setInterval(() => {
+        //console.log("Send to " + wss.clients.length + " Clients")
+
+        //Wenn es keine Clients gibt
+        if (wss.clients.length == 0) {
+            console.log("No more Clients for Timer")
+
+            //Timer zuruecksetzen
+            clearInterval(timerID);
+            timerID = null;
         }
 
+        //Aktuelle Zeit in Titel ermitteln
         let time = execSync('mocp -Q %ct');
-        console.log(time.toString());
+        console.log(time.toString().trim())
 
-        let file = execSync('mocp -Q %file');
-        console.log(file.toString());
+        //MessageObj erstellen
+        let messageObj = {
+            type: "time",
+            value: time.toString().trim()
+        };
 
-        //durch Liste der Sockets gehen
-        for (const s of sockets) {
-            //Message an Socket schicken
-            s.emit('send-file-message', file.toString());
-
-            //Message an Socket schicken
-            s.emit('send-time-message', time.toString());
+        //MessageObj an WS senden
+        for (ws of wss.clients) {
+            ws.send(JSON.stringify(messageObj));
         }
-    }, 1000);
+    }, 1000)
 }
-
-//Auf Port 8080 lauschen
-http.listen(8080, () => {
-    console.log('started on port 8080');
-});
