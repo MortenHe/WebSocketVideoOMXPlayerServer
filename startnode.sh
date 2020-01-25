ad442d91309a2ab6ece9f8750a3fc9c100ebb72e
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-01-26 00:02:15 +0100

    activateVideoApp

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/pi/mh_prog/VideoServer
-/usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-server.txt &
+/usr/bin/sudo /usr/bin/node /home/pi/mh_prog/VideoServer/server.js > /home/pi/mh_prog/output-server.txt &
